5ef2e597d5e3106fb5e95aed3b219a243fbb7e9a
fqq <2998657166@qq.com>
2023-05-30 00:21:29 +0800

    优化了代码

--- src/main/java/com/example/mqtt/client/MqttService.java
+++ src/main/java/com/example/mqtt/client/MqttService.java
@@ -23,7 +23,8 @@ public class MqttService {
      * @param pushMessage 消息体
      */
     public void publish(int qos, boolean retained, String topic, String pushMessage) throws MqttException {
-        log.info("【mqtt】发布主题：" + topic);
+        log.info("【mqtt】发布主题：{}", topic);
+        log.info("【mqtt】发布消息：{}", pushMessage);
         MqttMessage message = new MqttMessage();
         message.setQos(qos);
         message.setRetained(retained);
@@ -43,7 +44,7 @@ public class MqttService {
      *              Qos3：只分发一次，确保消息送达且只传递一次
      */
     public void subscribe(String topic, int qos) throws MqttException {
-        log.info("【mqtt】：订阅了主题 topic: {}", topic);
+        log.info("【mqtt】：订阅了主题 topic: {}, qos: {}", topic, qos);
         IMqttToken token = CustomMqttClient.getClient().subscribe(topic, qos);
         token.waitForCompletion();
     }
@@ -51,15 +52,16 @@ public class MqttService {
     /**
      * 功能描述: 订阅某些主题
      *
-     * @param topic 主题
-     * @param qos   消息质量
-     *              Qos1：消息发送一次，不确保
-     *              Qos2：至少分发一次，服务器确保接收消息进行确认
-     *              Qos3：只分发一次，确保消息送达和只传递一次
+     * @param topics 主题
+     * @param qos    消息质量
+     *               Qos1：消息发送一次，不确保
+     *               Qos2：至少分发一次，服务器确保接收消息进行确认
+     *               Qos3：只分发一次，确保消息送达和只传递一次
      */
-    public void subscribe(String[] topic, int[] qos) throws MqttException {
-        log.info("【mqtt】：订阅了主题 topic: {}", Arrays.toString(topic));
-        IMqttToken token = CustomMqttClient.getClient().subscribe(topic, qos);
+    public void subscribe(String[] topics, int[] qos) throws MqttException {
+        log.info("【mqtt】：订阅了主题 topics: {}", Arrays.toString(topics));
+        log.info("【mqtt】qos: {}", Arrays.toString(qos));
+        IMqttToken token = CustomMqttClient.getClient().subscribe(topics, qos);
         token.waitForCompletion();
     }
 }
